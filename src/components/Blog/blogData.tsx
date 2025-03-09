ca0452f575bb74be1712c42803cc6111e2b2f05d
ALI RAMADHANI <danr27498@gmail.com>
2025-03-09 21:22:17 +0700

    perbarui blog grid page

--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -3,44 +3,89 @@ import { Blog } from "@/types/blog";
 const blogData: Blog[] = [
   {
     id: 1,
-    title: "Best UI components for modern websites",
+    title: "Bahan Ramah Lingkungan yang fashionable",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "Mengenal jenis kain yang lebih ramah lingkungan dibandingkan bahan sintetis biasa.",
     image: "/images/blog/blog-01.jpg",
     author: {
-      name: "Samuyl Joshi",
+      name: "Avriel Adi",
       image: "/images/blog/author-01.png",
-      designation: "Graphic Designer",
+      designation: "Desainer lokal",
     },
     tags: ["creative"],
     publishDate: "2025",
   },
   {
     id: 2,
-    title: "9 simple ways to improve your design skills",
+    title: "Daur Ulang Pakaian Lama: Kreativitas Fashion",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "Inspirasi mengolah kembali pakaian bekas agar tetap stylish dan ramah lingkungan.",
     image: "/images/blog/blog-02.jpg",
     author: {
-      name: "Musharof Chy",
+      name: "Hamka Zainul Ardhi",
       image: "/images/blog/author-02.png",
-      designation: "Content Writer",
+      designation: "Desainer Internasional",
     },
-    tags: ["computer"],
+    tags: ["creative"],
     publishDate: "2025",
   },
   {
     id: 3,
-    title: "Tips to quickly improve your coding speed.",
+    title: "Sustainable Fashion: Tren atau Gaya Hidup?",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "Menelusuri perkembangan sustainable fashion di Indonesia dan tantangan industrinya.",
     image: "/images/blog/blog-03.jpg",
     author: {
-      name: "Lethium Deo",
+      name: "Ali Ramadhani",
       image: "/images/blog/author-03.png",
-      designation: "Graphic Designer",
+      designation: "Desainer lokal",
+    },
+    tags: ["fashion"],
+    publishDate: "2025",
+  },
+
+  {
+    id: 4,
+    title: "Swap, Thrift, atau Upcycle?",
+    paragraph:
+      "Membahas berbagai cara mengurangi limbah fashion dengan metode yang sesuai kebutuhan.",
+    image: "/images/blog/blog-04.jpg",
+    author: {
+      name: "Ali Ramadhani",
+      image: "/images/blog/author-03.png",
+      designation: "Desainer lokal",
     },
     tags: ["design"],
+    publishDate: "2025",
+  },
+
+  {
+    id: 5,
+    title: "5 Cara Mudah Memulai Gaya Hidup Sustainable Fashion",
+    paragraph:
+      "Tips praktis untuk mulai menerapkan gaya berpakaian yang lebih berkelanjutan.",
+    image: "/images/blog/blog-05.jpg",
+    author: {
+      name: "Ali Ramadhani",
+      image: "/images/blog/author-03.png",
+      designation: "Desainer lokal",
+    },
+    tags: ["fashion"],
+    publishDate: "2025",
+  },
+
+  {
+    id: 6,
+    title: "Slow Fashion vs Fast Fashion",
+    paragraph:
+      "Membahas perbedaan slow fashion dan fast fashion serta dampaknya terhadap lingkungan.",
+    image: "/images/blog/blog-06.jpg",
+    author: {
+      name: "Ali Ramadhani",
+      image: "/images/blog/author-03.png",
+      designation: "Desainer lokal",
+    },
+    tags: ["fashion"],
     publishDate: "2025",
   },
 ];
